e1191e444e27e9c5c9d97a1d29cc09728aa78aba
Dan Knightsaaf <dknights@umn.edu>
2016-01-27 14:22:49 -0600

    Added instructions for problems with installation and for MSI, and for running basic QIIME analyses on class data

---
+++ data/globalgut/readme.txt
@@ -0,0 +1,18 @@
+# global_gut_200k.fna: subsampled sequences from global gut study
+# map.txt: simplified metadata from global gut study
+# otutable.biom: closed-reference OTU table from global gut study
+
+# to run core QIIME analyses:
+
+# pick de novo OTUs 
+time pick_de_novo_otus.py -i global_gut_200k.fna -o otus_de_novo
+
+# run core QIIME diversity analyses on de novo OTU table
+time core_diversity_analyses.py -i otus_de_novo/otu_table.biom -m map.txt -o corediv_dn -e 1000 --tree_fp otus_de_novo/rep_set.tre
+
+# pick closed reference OTUs
+time pick_closed_reference_otus.py -i global_gut_200k.fna -o otus_closed_ref
+cp otus_closed_ref/otu_table.biom otutable.biom
+
+# run core QIIME diversity analyses on closed-reference OTU table
+time core_diversity_analyses.py -i otus_closed_ref/otu_table.biom -m map.txt -o corediv_cr -e 1000 --tree_fp otus_closed_ref/97_otus.tree
--- doc/make.r
+++ doc/make.r
@@ -1,8 +1,29 @@
 # Renders all RMD files
 #
-# run with 
+# USAGE:
+#
+# To make all RMD to html and pdf, run with 
 # Rscript make.r
+# 
+# To make specific rmd files, run with
+# Rscript make.r file1.rmd file2.rmd ...
+
 library('rmarkdown')
-rmarkdown::render('01.installing.qiime.rmd', output_format=c("html_document", "pdf_document"))
-rmarkdown::render('02.loading.data.rmd', output_format=c("html_document", "pdf_document"))
-rmarkdown::render('index.rmd', output_format=c("html_document"))
+args <- commandArgs(trailing=TRUE)
+
+# if any RMD files were passed in the command line,
+# process them
+
+if(length(args) == 0){
+    # if no args passed, find all rmd files
+    args <- c(Sys.glob("*.rmd"), Sys.glob("*.RMD"))
+}
+
+for(arg in args){
+    # Check that all args are rmd files
+    if(!grepl(".rmd$",arg)){
+        stop(paste('Non-RMD file passed on command line:',arg))
+    }
+    cat("Rendering HTML and PDF for input file",arg,"\n")
+    rmarkdown::render(arg, output_format=c("html_document", "pdf_document"))
+}
