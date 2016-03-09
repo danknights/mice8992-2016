cad91c2e3fcf4466575a35f60585016a9d825cdd
Dan Knightsaaf <dknights@umn.edu>
2016-03-09 16:06:05 -0600

    LGD now handles dist objects

--- src/local.gradient.distance.r
+++ src/local.gradient.distance.r
@@ -1,5 +1,6 @@
 "lgd" <- function(d,neighborhood.size=4,weighted=TRUE) {
     require('igraph')
+    d <- as.matrix(d)
     dd <- matrix(0,nrow(d), nrow(d))
     for(i in 1:nrow(dd)) dd[i,order(d[i,])[1:(neighborhood.size+1)]] <- 1
     if(weighted) dd[dd>0] <- d[dd>0] else weighted <- NULL
