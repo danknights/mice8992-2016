a6e73208f375a8d282042b9cd0d529302a134356
Dan Knightsaaf <dknights@umn.edu>
2016-03-09 09:30:54 -0600

    added local gradient distance funcdtion

---
+++ src/local.gradient.distance.r
@@ -0,0 +1,8 @@
+"lgd" <- function(d,neighborhood.size=4,weighted=TRUE) {
+    require('igraph')
+    dd <- matrix(0,nrow(d), nrow(d))
+    for(i in 1:nrow(dd)) dd[i,order(d[i,])[1:(neighborhood.size+1)]] <- 1
+    if(weighted) dd[dd>0] <- d[dd>0] else weighted <- NULL
+    g <- graph.adjacency(dd,weighted=weighted)
+    return(shortest.paths(g))
+}
