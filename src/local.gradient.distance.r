84ae2c72e68e3a51549fbffc2d2625607338392c
Dan Knightsaaf <dknights@umn.edu>
2016-03-10 12:59:03 -0600

    added graph function to lgd

--- src/local.gradient.distance.r
+++ src/local.gradient.distance.r
@@ -1,9 +1,16 @@
-"lgd" <- function(d,neighborhood.size=4,weighted=TRUE) {
+"lg.dist" <- function(d,neighborhood.size=4, weighted=TRUE) {
+    g <- lg.graph(d, neighborhood.size=neighborhood.size, weighted=weighted)
+    lgd <- shortest.paths(g)
+    return(lgd)
+}
+
+"lg.graph" <- function(d,neighborhood.size=4,weighted=TRUE) {
     require('igraph')
     d <- as.matrix(d)
     dd <- matrix(0,nrow(d), nrow(d))
     for(i in 1:nrow(dd)) dd[i,order(d[i,])[1:(neighborhood.size+1)]] <- 1
     if(weighted) dd[dd>0] <- d[dd>0] else weighted <- NULL
-    g <- graph.adjacency(dd,weighted=weighted)
-    return(shortest.paths(g))
+
+    g <- graph.adjacency(dd,weighted=weighted, mode='undirected')
+    return(g)
 }
